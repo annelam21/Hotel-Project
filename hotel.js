ba6f1e8823965bf1156b3bea9a1f9a4f43efbacb
loelam21 <42423893+loelam21@users.noreply.github.com>
2018-11-01 16:37:18 -1000

    Update hotel.js

--- hotel.js
+++ hotel.js
@@ -1,14 +1,16 @@
 var roomOneTotal = 0;
 
 inputOne.onkeyup = function() { //when key is pressed while in input field, run function
-  if (event.keyCode == 13) { //if 13 (aka enter) is hit...
+  if (event.keyCode == 13) { //if 13(enter) is hit...
     addToRoomOne(); //run function
     previousGuestsRoomOne(); //run function
   }
 }
+
 function addToRoomOne() {
   if (roomOneTotal < 4)
   {
+    //add guest name to list
     var li = document.createElement("li"); //create list element
     var newText = document.createTextNode(inputOne.value); //create text node from user's input
     li.appendChild(newText);
@@ -22,11 +24,12 @@ function addToRoomOne() {
     var spanElem = document.createElement('span'); //create span element
     spanElem.appendChild(textTime);
     li.appendChild(spanElem);
-      
+    
+    //remove guest
     li.onclick = function() { //when li is clicked...
       this.parentNode.removeChild(this); //find li's parent node to remove the li
       roomOneTotal--; //subtract one from variable
-      document.getElementById('fullOne').innerHTML = ""; //removes "Room full."
+      document.getElementById('fullOne').innerHTML = ""; //removes "Room full!"
     }
   }
 else
@@ -37,16 +40,18 @@ else
     createElem.appendChild(fullText);
   }
 }
+
 function removeAllRoomOne() {
   var ol = document.getElementById("guestListOne");
   var olChildren = ol.childNodes;
   for (var i = olChildren.length; i > 0; i--) { //if ol has a child...
     ol.removeChild(olChildren[i - 1]); //...remove the child. Minus 1 bc index position
-    document.getElementById('fullOne').innerHTML = ""; //removes "Room full."
+    document.getElementById('fullOne').innerHTML = ""; //removes "Room full!"
   }
   
   roomOneTotal = 0; //reset variable
 }
+
 function previousGuestsRoomOne() { //display a list of all guests that have checked in since the script has been running
   if (roomOneTotal < 4)
   {
@@ -70,44 +75,9 @@ function previousGuestsRoomOne() { //display a list of all guests that have chec
 }
 
 function lockRoomOne() {
-  //delete all elements with class of "one" and have text saying "locked for cleaning"
+  //hide the room
   var callContain = document.getElementById("containerOne"); //getting everything for room 1
   callContain.style.display = 'none'; //...and hides it
-  
-  //display "Locked for cleaning text"
-  var createElem = document.createElement("h2");
-  createElem.className = "clean"; //gives h2 element a class
-  createElem.id = "cleaningText";
-  var newText = document.createTextNode("Room One locked for cleaning.");
-  createElem.appendChild(newText);
-  var callDiv = document.getElementById('roomIchi');
-  callDiv.appendChild(createElem);
-  
-  var spacing = document.createElement('br');
-  spacing.id = "break";
-  callDiv.appendChild(spacing);
-  
-  var createButton = document.createElement('button');
-  createButton.className = "locks";
-  createButton.id = "unlockButton";
-  createButton.onclick = function() { //this function "unlocks" the room when button is clicked
-    var callContain = document.getElementById("containerOne"); //getting everything for room 1
-    callContain.style.display = 'inline'; //...and display it
-    
-    //delete the button, "Locked for cleaning", & breaks
-    var h2Elem = document.getElementById('cleaningText');
-    var callDiv = document.getElementById('roomIchi');
-    callDiv.removeChild(h2Elem);
-    var button = document.getElementById('unlockButton');
-    callDiv.removeChild(button);
-    var breaks = document.getElementById('break');
-    callDiv.removeChild(breaks);
-  };
-  var buttonText = document.createTextNode("Unlock Room");
-  createButton.appendChild(buttonText);
-  callDiv.appendChild(createButton);
-  
-  callDiv.appendChild(spacing);//adds a br
   
   //removes all guests who are checked in
   var ol = document.getElementById("guestListOne");
@@ -117,6 +87,20 @@ function lockRoomOne() {
     document.getElementById('fullOne').innerHTML = ""; //removes "Room full."
   }
   roomOneTotal = 0; //reset variable
+  
+  //display locked text
+  var callLockedTextDiv = document.getElementById('lockedRoomOne'); //getting everything for room 1
+  callLockedTextDiv.style.display = 'inline'; //...and display it
+}
+
+function unlockRoomOne() {
+  //display the room
+  var callContain = document.getElementById('containerOne'); //getting everything for room 1
+  callContain.style.display = 'inline'; //...and display it
+  
+  //hide the: unlock button, locked text, and br
+  var callLockedTextDiv = document.getElementById('lockedRoomOne');
+  callLockedTextDiv.style.display = 'none';
 }
 
 var roomTwoTotal = 0;
@@ -191,44 +175,9 @@ function previousGuestsRoomTwo() { //display a list of all guests that have chec
 }
 
 function lockRoomTwo() {
-  //delete all elements with class of "two" and have text saying "locked for cleaning"
-  var callContain = document.getElementById("containerTwo"); //getting everything for room 1
+  //hide the room
+  var callContain = document.getElementById("containerTwo"); //getting everything for room 2
   callContain.style.display = 'none'; //...and hides it
-  
-  //display "Locked for cleaning text"
-  var createElem = document.createElement("h2");
-  createElem.className = "clean"; //gives h2 element a class
-  createElem.id = "cleaningText";
-  var newText = document.createTextNode("Room Two locked for cleaning.");
-  createElem.appendChild(newText);
-  var callDiv = document.getElementById('roomNi');
-  callDiv.appendChild(createElem);
-  
-  var spacing = document.createElement('br');
-  spacing.id = "break";
-  callDiv.appendChild(spacing);
-  
-  var createButton = document.createElement('button');
-  createButton.className = "locks";
-  createButton.id = "unlockButton";
-  createButton.onclick = function() { //this function "unlocks" the room when button is clicked
-    var callContain = document.getElementById("containerTwo"); //getting everything for room 1
-    callContain.style.display = 'inline'; //...and display it
-    
-    //delete: the button, "Locked for cleaning", & breaks
-    var h2Elem = document.getElementById('cleaningText');
-    var callDiv = document.getElementById('roomNi');
-    callDiv.removeChild(h2Elem);
-    var button = document.getElementById('unlockButton');
-    callDiv.removeChild(button);
-    var breaks = document.getElementById('break');
-    callDiv.removeChild(breaks);
-  };
-  var buttonText = document.createTextNode("Unlock Room");
-  createButton.appendChild(buttonText);
-  callDiv.appendChild(createButton);
-  
-  callDiv.appendChild(spacing);//adds a br
   
   //removes all guests who are checked in
   var ol = document.getElementById("guestListTwo");
@@ -238,6 +187,20 @@ function lockRoomTwo() {
     document.getElementById('fullTwo').innerHTML = ""; //removes "Room full."
   }
   roomTwoTotal = 0; //reset variable
+  
+  //display locked text
+  var callLockedTextDiv = document.getElementById('lockedRoomTwo'); //getting everything for room 2
+  callLockedTextDiv.style.display = 'inline'; //...and display it
+}
+
+function unlockRoomTwo() {
+  //display the room
+  var callContain = document.getElementById('containerTwo'); //getting everything for room 2
+  callContain.style.display = 'inline'; //...and display it
+  
+  //hide the: unlock button, locked text, and br
+  var callLockedTextDiv = document.getElementById('lockedRoomTwo');
+  callLockedTextDiv.style.display = 'none';
 }
 
 var roomThreeTotal = 0;
@@ -312,44 +275,9 @@ function previousGuestsRoomThree() { //display a list of all guests that have ch
 }
 
 function lockRoomThree() {
-  //delete all elements with class of "three" and have text saying "locked for cleaning"
-  var callContain = document.getElementById("containerThree"); //getting everything for room 1
+  //hide the room
+  var callContain = document.getElementById("containerThree"); //getting everything for room 3
   callContain.style.display = 'none'; //...and hides it
-  
-  //display "Locked for cleaning text"
-  var createElem = document.createElement("h2");
-  createElem.className = "clean"; //gives h2 element a class
-  createElem.id = "cleaningText";
-  var newText = document.createTextNode("Room Three locked for cleaning.");
-  createElem.appendChild(newText);
-  var callDiv = document.getElementById('roomSan');
-  callDiv.appendChild(createElem);
-  
-  var spacing = document.createElement('br');
-  spacing.id = "break";
-  callDiv.appendChild(spacing);
-  
-  var createButton = document.createElement('button');
-  createButton.className = "locks";
-  createButton.id = "unlockButton";
-  createButton.onclick = function() { //this function "unlocks" the room when button is clicked
-    var callContain = document.getElementById("containerThree"); //getting everything for room 1
-    callContain.style.display = 'inline'; //...and display it
-    
-    //delete the button, "Locked for cleaning", & breaks
-    var h2Elem = document.getElementById('cleaningText');
-    var callDiv = document.getElementById('roomSan');
-    callDiv.removeChild(h2Elem);
-    var button = document.getElementById('unlockButton');
-    callDiv.removeChild(button);
-    var breaks = document.getElementById('break');
-    callDiv.removeChild(breaks);
-  };
-  var buttonText = document.createTextNode("Unlock Room");
-  createButton.appendChild(buttonText);
-  callDiv.appendChild(createButton);
-  
-  callDiv.appendChild(spacing);//adds a br
   
   //removes all guests who are checked in
   var ol = document.getElementById("guestListThree");
@@ -359,6 +287,20 @@ function lockRoomThree() {
     document.getElementById('fullThree').innerHTML = ""; //removes "Room full."
   }
   roomThreeTotal = 0; //reset variable
+  
+  //display locked text
+  var callLockedTextDiv = document.getElementById('lockedRoomThree'); //getting everything for room 3
+  callLockedTextDiv.style.display = 'inline'; //...and display it
+}
+
+function unlockRoomThree() {
+  //display the room
+  var callContain = document.getElementById('containerThree'); //getting everything for room 3
+  callContain.style.display = 'inline'; //...and display it
+  
+  //hide the: unlock button, locked text, and br
+  var callLockedTextDiv = document.getElementById('lockedRoomThree');
+  callLockedTextDiv.style.display = 'none';
 }
 
 var roomFourTotal = 0;
@@ -433,44 +375,9 @@ function previousGuestsRoomFour() { //display a list of all guests that have che
 }
 
 function lockRoomFour() {
-  //delete all elements with class of "four" and have text saying "locked for cleaning"
+  //hide the room
   var callContain = document.getElementById("containerFour"); //getting everything for room 1
   callContain.style.display = 'none'; //...and hides it
-  
-  //display "Locked for cleaning text"
-  var createElem = document.createElement("h2");
-  createElem.className = "clean"; //gives h2 element a class
-  createElem.id = "cleaningText";
-  var newText = document.createTextNode("Room Four locked for cleaning.");
-  createElem.appendChild(newText);
-  var callDiv = document.getElementById('roomYon');
-  callDiv.appendChild(createElem);
-  
-  var spacing = document.createElement('br');
-  spacing.id = "break";
-  callDiv.appendChild(spacing);
-  
-  var createButton = document.createElement('button');
-  createButton.className = "locks";
-  createButton.id = "unlockButton";
-  createButton.onclick = function() { //this function "unlocks" the room when button is clicked
-    var callContain = document.getElementById("containerFour"); //getting everything for room 1
-    callContain.style.display = 'inline'; //...and display it
-    
-    //delete the button, "Locked for cleaning", & breaks
-    var h2Elem = document.getElementById('cleaningText');
-    var callDiv = document.getElementById('roomYon');
-    callDiv.removeChild(h2Elem);
-    var button = document.getElementById('unlockButton');
-    callDiv.removeChild(button);
-    var breaks = document.getElementById('break');
-    callDiv.removeChild(breaks);
-  };
-  var buttonText = document.createTextNode("Unlock Room");
-  createButton.appendChild(buttonText);
-  callDiv.appendChild(createButton);
-  
-  callDiv.appendChild(spacing);//adds a br
   
   //removes all guests who are checked in
   var ol = document.getElementById("guestListFour");
@@ -480,6 +387,20 @@ function lockRoomFour() {
     document.getElementById('fullFour').innerHTML = ""; //removes "Room full."
   }
   roomFourTotal = 0; //reset variable
+  
+  //display locked text
+  var callLockedTextDiv = document.getElementById('lockedRoomFour'); //getting everything for room 4
+  callLockedTextDiv.style.display = 'inline'; //...and display it
+}
+
+function unlockRoomFour() {
+  //display the room
+  var callContain = document.getElementById('containerFour'); //getting everything for room 4
+  callContain.style.display = 'inline'; //...and display it
+  
+  //hide the: unlock button, locked text, and br
+  var callLockedTextDiv = document.getElementById('lockedRoomFour');
+  callLockedTextDiv.style.display = 'none';
 }
 
 var roomFiveTotal = 0;
@@ -554,44 +475,9 @@ function previousGuestsRoomFive() { //display a list of all guests that have che
 }
 
 function lockRoomFive() {
-  //delete all elements with class of "five" and have text saying "locked for cleaning"
+  //hide the room
   var callContain = document.getElementById("containerFive"); //getting everything for room 1
   callContain.style.display = 'none'; //...and hides it
-  
-  //display "Locked for cleaning text"
-  var createElem = document.createElement("h2");
-  createElem.className = "clean"; //gives h2 element a class
-  createElem.id = "cleaningText";
-  var newText = document.createTextNode("Room Five locked for cleaning.");
-  createElem.appendChild(newText);
-  var callDiv = document.getElementById('roomGo');
-  callDiv.appendChild(createElem);
-  
-  var spacing = document.createElement('br');
-  spacing.id = "break";
-  callDiv.appendChild(spacing);
-  
-  var createButton = document.createElement('button');
-  createButton.className = "locks";
-  createButton.id = "unlockButton";
-  createButton.onclick = function() { //this function "unlocks" the room when button is clicked
-    var callContain = document.getElementById("containerFive"); //getting everything for room 1
-    callContain.style.display = 'inline'; //...and display it
-    
-    //delete the button, "Locked for cleaning", & breaks
-    var h2Elem = document.getElementById('cleaningText');
-    var callDiv = document.getElementById('roomGo');
-    callDiv.removeChild(h2Elem);
-    var button = document.getElementById('unlockButton');
-    callDiv.removeChild(button);
-    var breaks = document.getElementById('break');
-    callDiv.removeChild(breaks);
-  };
-  var buttonText = document.createTextNode("Unlock Room");
-  createButton.appendChild(buttonText);
-  callDiv.appendChild(createButton);
-  
-  callDiv.appendChild(spacing);//adds a br
   
   //removes all guests who are checked in
   var ol = document.getElementById("guestListFive");
@@ -601,6 +487,20 @@ function lockRoomFive() {
     document.getElementById('fullFive').innerHTML = ""; //removes "Room full."
   }
   roomFiveTotal = 0; //reset variable
+  
+  //display locked text
+  var callLockedTextDiv = document.getElementById('lockedRoomFive'); //getting everything for room 5
+  callLockedTextDiv.style.display = 'inline'; //...and display it
+}
+
+function unlockRoomFive() {
+  //display the room
+  var callContain = document.getElementById('containerFive'); //getting everything for room 5
+  callContain.style.display = 'inline'; //...and display it
+  
+  //hide the: unlock button, locked text, and br
+  var callLockedTextDiv = document.getElementById('lockedRoomFive');
+  callLockedTextDiv.style.display = 'none';
 }
 
 var roomSixTotal = 0;
@@ -675,44 +575,9 @@ function previousGuestsRoomSix() { //display a list of all guests that have chec
 }
 
 function lockRoomSix() {
-  //delete all elements with class of "six" and have text saying "locked for cleaning"
+  //hide the room
   var callContain = document.getElementById("containerSix"); //getting everything for room 1
   callContain.style.display = 'none'; //...and hides it
-  
-  //display "Locked for cleaning text"
-  var createElem = document.createElement("h2");
-  createElem.className = "clean"; //gives h2 element a class
-  createElem.id = "cleaningText";
-  var newText = document.createTextNode("Room Six locked for cleaning.");
-  createElem.appendChild(newText);
-  var callDiv = document.getElementById('roomRoku');
-  callDiv.appendChild(createElem);
-  
-  var spacing = document.createElement('br');
-  spacing.id = "break";
-  callDiv.appendChild(spacing);
-  
-  var createButton = document.createElement('button');
-  createButton.className = "locks";
-  createButton.id = "unlockButton";
-  createButton.onclick = function() { //this function "unlocks" the room when button is clicked
-    var callContain = document.getElementById("containerSix"); //getting everything for room 1
-    callContain.style.display = 'inline'; //...and display it
-    
-    //delete the button, "Locked for cleaning", & breaks
-    var h2Elem = document.getElementById('cleaningText');
-    var callDiv = document.getElementById('roomRoku');
-    callDiv.removeChild(h2Elem);
-    var button = document.getElementById('unlockButton');
-    callDiv.removeChild(button);
-    var breaks = document.getElementById('break');
-    callDiv.removeChild(breaks);
-  };
-  var buttonText = document.createTextNode("Unlock Room");
-  createButton.appendChild(buttonText);
-  callDiv.appendChild(createButton);
-  
-  callDiv.appendChild(spacing);//adds a br
   
   //removes all guests who are checked in
   var ol = document.getElementById("guestListSix");
@@ -722,4 +587,18 @@ function lockRoomSix() {
     document.getElementById('fullSix').innerHTML = ""; //removes "Room full."
   }
   roomSixTotal = 0; //reset variable
+  
+  //display locked text
+  var callLockedTextDiv = document.getElementById('lockedRoomSix'); //getting everything for room 6
+  callLockedTextDiv.style.display = 'inline'; //...and display it
+}
+
+function unlockRoomSix() {
+  //display the room
+  var callContain = document.getElementById('containerSix'); //getting everything for room 6
+  callContain.style.display = 'inline'; //...and display it
+  
+  //hide the: unlock button, locked text, and br
+  var callLockedTextDiv = document.getElementById('lockedRoomSix');
+  callLockedTextDiv.style.display = 'none';
 }
